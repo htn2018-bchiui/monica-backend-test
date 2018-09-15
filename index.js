dc1706f93f6b8f99d8e9ddb8f8c6e24eace3b25c
bui1@ualberta.ca <bui1@ualberta.ca>
2018-09-15 10:27:48 -0400

    got initial post request message to send from twilio to personal number

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
 // https://www.twilio.com/docs/libraries/node/usage-guide
 
 const express = require("express"); 
+const app = express();
 require('dotenv').config(); // for env variables
 
 const port = process.env.PORT || 3000;
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express();
 var accountSid = process.env.ACCOUNTSID;
 var authToken = process.env.AUTHTOKEN;
+const TWILIONUM = process.env.TWILIONUM;
+const PHONENUM = process.env.PHONENUM;
 
 const client = require('twilio')(accountSid, authToken);
+const bodyParser = require("body-parser");
 
-const TWILIONUM = process.env.TWILIONUM;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send(`${process.env.TEST}`); // testing if environment varaibles work
 });
 
-// webhook works
+// webhook works 
+// we send message then twilio replies
+// could be used as a confirmation for when we send robot the message
 app.post('/sms', (req, res) => {
   // Start our TwiML response.
   const twiml = new MessagingResponse();
@@ -34,7 +40,24 @@ app.post('/sms', (req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   console.log(twiml.toString());
+  console.log(req.body); // to see the extra details the robot will send to the personal number
   res.end(twiml.toString());
+});
+
+// post request from twilio to personal number
+app.post("/mms", (req,res) => {
+  client.messages
+    .create({
+      body: 'The lego man',
+      from: TWILIONUM,
+      mediaUrl: 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
+      to: PHONENUM
+    })
+    .then(message => {
+      console.log(message.sid);
+      console.log(message.subresourceUris.media);
+    })
+    .done();
 });
 
 app.listen(port, () => {
