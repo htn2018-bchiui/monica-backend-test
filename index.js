1d72b70249aa860aabcdfa68feb4163992428341
bui1@ualberta.ca <bui1@ualberta.ca>
2018-09-15 09:03:42 -0400

    got that webhook working :)

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require("express");
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express();
+var accountSid = process.env.ACCOUNTSID;
+var authToken = process.env.AUTHTOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+const TWILIONUM = "+18077870567";
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`${process.env.TEST}`); // testing if environment varaibles work
 });
 
+// webhook works
+app.post('/sms', (req, res) => {
+  // Start our TwiML response.
+  const twiml = new MessagingResponse();
 
-app.listen(3000, () => {
+  // Add a text message.
+  const msg = twiml.message('Check out this sweet owl!');
+
+  // Add a picture message.
+  msg.media('https://demo.twilio.com/owl.png');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  console.log(twiml.toString());
+  res.end(twiml.toString());
+});
+
+app.listen(port, () => {
   console.log('Example app listening on port 3000!');
 });
 
